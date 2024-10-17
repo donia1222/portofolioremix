ea4a93234de3fdd2dd735d5571b855458f32b9c4
lwev <106478077+lwev@users.noreply.github.com>
2024-10-17 16:10:19 +0200

    “j”

--- app/data/blogPosts.ts
+++ app/data/blogPosts.ts
@@ -16,7 +16,19 @@ export const blogPosts: BlogPost[] = [
     date: "2024-10-17",
     content: `
       <p>Erstellen Sie Ihre eigene Website ohne Programmierkenntnisse ab nur 290 Franken. 
-      Mit unserem benutzerfreundlichen Admin-Panel können Sie Bilder, Texte, Farben und das gesamte Design Ihrer Website ganz einfach anpassen.</p>`,
+      Mit unserem benutzerfreundlichen Admin-Panel können Sie Bilder, Texte, Farben und das gesamte Design Ihrer Website ganz einfach anpassen.</p>
+      <br>  </br>
+      <p><strong>Vorteile:</strong></p>
+      <ul>
+        <li>Domain und Server inklusive.</li>
+        <li>2 personalisierte E-Mail-Konten.</li>
+        <li>Volle Kontrolle über Design und Inhalte.</li>
+        <li>Einfache Bedienung, ohne technische Kenntnisse.</li>
+      </ul>
+      <br>  </br>
+      <p>Möchten Sie testen, wie es funktioniert?</p>
+      <p><a href="https://lweb.ch/web.html" target="_blank" class="text-indigo-500 underline">Mehr Infos</a></p>
+    `,
     imageUrl: "https://lweb.ch/images/Captgura%20dXe%20pantalla%202024-09-03%20a%20las%2023.00.26.png",
     views: 10,
   },
