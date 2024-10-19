976a664fc819f5e720b3f4c39f9aa57210f23882
lwev <106478077+lwev@users.noreply.github.com>
2024-10-19 10:30:17 +0200

    “l”

--- app/root.tsx
+++ app/root.tsx
@@ -24,14 +24,13 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
-        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body>
         {children}
