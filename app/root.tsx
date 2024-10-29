d4b879ee55595e2966a4cff4ded20f006beb1dbc
lwev <106478077+lwev@users.noreply.github.com>
2024-10-29 14:56:20 +0100

    “gitdxdxh”

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 
 import "./tailwind.css";
 
@@ -18,29 +18,34 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
   },
+  { rel: "icon", href: "/favicon.ico" },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const meta: MetaFunction = () => {
+  return [
+    { charset: "utf-8" },
+    { title: "Lweb" },
+    { name: "description", content: "lweb schweiz" },
+    { name: "keywords", content: "Moderne Webssiten, App-Entwicklung st gallen, App-Entwicklung zurich" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "theme-color", content: "#000000" },
+  ];
+};
+
+export default function App() {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="es" data-theme="light">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <meta name="theme-color" content="#000000" />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
