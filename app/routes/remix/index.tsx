a0b4e35e1cfff7879a294e6c328153914bdee91e
lwev <106478077+lwev@users.noreply.github.com>
2024-10-29 02:13:37 +0100

    “custofmxvsfsisc”

--- app/routes/remix/index.tsx
+++ app/routes/remix/index.tsx
@@ -61,7 +61,7 @@ export default function Index() {
       </header>
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         {components.map(({ name, Component, code }, index) => (
-          <div key={index} className="mb-12 bg-white bg-opacity-80 shadow-lg overflow-hidden sm:rounded-lg transition-all hover:shadow-xl">
+          <div key={index} className="mb-12 bg-white bg-opacity-90 shadow-lg overflow-hidden sm:rounded-lg transition-all hover:shadow-xl">
             <div className="px-4 py-5 sm:px-6">
               <h2 className="text-xl leading-6 font-semibold text-gray-900">{name}</h2>
             </div>
