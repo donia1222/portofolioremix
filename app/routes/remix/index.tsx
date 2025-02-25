4a2cb1f1ef1f7d0cc6f1174fd19a1112c232e34b
lwev <106478077+lwev@users.noreply.github.com>
2025-02-25 12:10:32 +0100

    “update21”

--- app/routes/remix/index.tsx
+++ app/routes/remix/index.tsx
@@ -43,7 +43,9 @@ export default function Component() {
 
   return (
     <div className="min-h-screen bg-animated-gradient bg-400% animate-gradientAnimation relative overflow-auto">
-         <Header />
+           <div className="relative z-[9999]">
+  <Header />
+</div>
 
       <header className="p-10 mt-40 ">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
